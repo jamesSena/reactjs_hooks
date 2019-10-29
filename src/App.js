2ab9178e0abece2a163243cc808a234d83219f24
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-29 15:44:48 -0300

    recebendo parametros na url via rota

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React , {useState, useEffect, useMemo, useCallback }from 'react';
 import Routes from './routes'
 function App() {
-const [tarefa, setTarefa]= useState([]);
-
-const [input, setInput] = useState('');
-
-
-useEffect(()=>{
-  const localStorageTarefa = localStorage.getItem('tarefas');
-  if(localStorageTarefa){
-    setTarefa(JSON.parse(localStorageTarefa));
-  }
-},[]);
-
-useEffect(()=>{
-  localStorage.setItem('tarefas', JSON.stringify(tarefa));
-},[tarefa]);
-
-const Adicionar = useCallback(() => {
-  setTarefa([... tarefa, input]);
-  setInput('');
-},[input, tarefa])
-
-const totalTarefas = useMemo(()=>tarefa.length,[tarefa]);
-
-
 
   return (
-    <Routes/>
-    
+    <Routes/>    
   );
 }
 
