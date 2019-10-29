f9e774acdfd718d7c0ab49811e5ee823abde6a39
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-10-28 22:18:58 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState}from 'react';
 
 function App() {
+const [tarefa, setTarefa]= useState([
+    'Estudar Node.js',
+    'Estudar ReactJS'
+
+]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          tarefa.map(t=>(
+            <li key={t}>{t}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
