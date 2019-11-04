ab80d8327ebf73780b618bc7f876455e2dcd1b25
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-11-04 17:40:25 -0300

    Melhorias no CSS

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {Title} from './styles';
+import React,{useState} from 'react';
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import {Container, From, SubmitButton} from './styles';
 
 export default function Main(){
+    const[newRepo, setNewRepo]= useState('');
+    function handleinputChange (e){
+        setNewRepo(e.target.value);
+    }
     return(
-        <Title>
-            Main
-            <span> TESTE</span>
-        </Title>
+        <Container>
+           <h1>
+               <FaGithub size={25}/>
+                Meus Repositorios
+           </h1>
+
+           <From onSubmit={()=>{}}>
+                <input 
+                    type="text" 
+                    placeholder="Adicionar"
+                    value={newRepo}
+                    onChange={handleinputChange}
+                    />
+                <SubmitButton >
+                    <FaPlus color="#FFF" size={14}/>
+                </SubmitButton>
+            </From>
+
+        </Container>
+
     );
 
 }
