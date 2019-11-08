266b64a0bb2f8c19a49cd4cf73f2078de662ec17
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-11-08 16:16:45 -0300

    realizando animação do botão

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {keyframes, css} from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -38,6 +38,18 @@ export const Form = styled.form`
 
 `;
 
+const animate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+  to{
+    transform: rotate(360deg);
+
+  }
+
+`;
+
+
 export const SubmitButton = styled.button.attrs((props)=>({
   type: 'submit',
   disabled: props.loading
@@ -55,4 +67,14 @@ export const SubmitButton = styled.button.attrs((props)=>({
     cursor:not-allowed;
     opacity:0.5;
   }
+  
+  ${ (props)=>props.loading &&
+      css`
+        svg{
+          animation: ${animate} 2s linear infinite;
+        }
+      `
+
+  }
+
 `;
