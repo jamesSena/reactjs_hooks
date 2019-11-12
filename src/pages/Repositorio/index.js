186356c817f53d50f800004e16a8e29eaafbb2ef
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-11-12 11:34:05 -0300

    Chamando a pagina de repositorio. Usando Promise para executar duas consultas. passando parametros nas consultas axios.

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Container} from './styles';
+import api from '../../services/api';
 
+export default function Repositorio({match}){
 
-export default function Repositorio(){
-    return(
-        <div>
-            <h1>Repositorio</h1>
-        </div>
-    );
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(()=> {
+    
+    async function load(){
+        //Nome do repositorio que ele digitou
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      //Executando duas consultas no mesmo tempo
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),//repositorioData
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params:{ //Passando parametros
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+      console.log(repositorioData.data);
+      console.log(issuesData.data);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+
+    }
+
+    load();
+
+  }, [match.params.repositorio]);
+
+  return(
+    <Container>
+
+    </Container>
+  )
 }
