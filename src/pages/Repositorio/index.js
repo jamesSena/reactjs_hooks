722fcb55341d11c2617e8bc48c95f0788d76e7c4
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-11-14 13:54:22 -0300

    Trabalhando com filters, adicionando três botões para o usuario escolher qual é a busca que a aplicação vai fazer

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Container, Owner, Loading, BackButton, IssuesList, PageActions} from './styles';
+import {Container, Owner, Loading, BackButton, IssuesList, PageActions, FilterList} from './styles';
 import { FaArrowLeft } from 'react-icons/fa';
 import api from '../../services/api';
 
@@ -9,6 +9,13 @@ export default function Repositorio({match}){
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [filters, setFilters] = useState([
+    {state: 'all', label: 'Todas', active: true},
+    {state: 'open', label: 'Abertas', active: false},
+    {state: 'closed', label: 'Fechadas', active: false},
+  ]);
+
+  const [filterIndex, setFilterIndex] = useState(0);
 
   useEffect(()=> {
     
@@ -21,7 +28,7 @@ export default function Repositorio({match}){
         api.get(`/repos/${nomeRepo}`),//repositorioData
         api.get(`/repos/${nomeRepo}/issues`, {
           params:{ //Passando parametros
-            state: 'open',
+            state:  filters.find(f => f.active).state,
             per_page: 5
           }
         })
@@ -37,7 +44,7 @@ export default function Repositorio({match}){
 
     load();
 
-  }, [match.params.repositorio]);
+  }, [filters, match.params.repositorio]);
 
   useEffect(()=> {
     console.log("Alterou o status do componente PAGE");
@@ -47,7 +54,7 @@ export default function Repositorio({match}){
 
       const response = await api.get(`/repos/${nomeRepo}/issues`, {
         params:{
-          state: 'open',
+          state: filters[filterIndex].state,
           page:page,
           per_page: 5,
         },
@@ -59,11 +66,15 @@ export default function Repositorio({match}){
 
     loadIssue();
 
-  }, [page]);
+  }, [filterIndex, filters, page]);
 
  
   function handlePage(action){
     setPage(action === 'back' ? page - 1 : page + 1 )
+  }
+
+  function handleFilter(index){
+    setFilterIndex(index);
   }
   //Se as informações ainda não estão carregadas, retornar uma mensagem
   if(loading){
@@ -91,7 +102,18 @@ export default function Repositorio({match}){
           <p>{repositorio.description}</p>
         </Owner>
 
-      
+        <FilterList active={filterIndex}>
+          {filters.map((filter, index) => (
+            <button
+             type="button"
+             key={filter.label}
+             onClick={()=> handleFilter(index)}
+            >
+              {filter.label}
+            </button>
+          ) )}
+        </FilterList>
+
         <IssuesList>
           {issues.map(issue => (//Exibindo a lista de problemas
             <li key={String(issue.id)}>
